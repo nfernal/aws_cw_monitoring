12d403a3e1f6b6b983be32a7b7395502da8179a3
Jonathan Simon <nfernal13@outlook.com>
2017-10-18 14:16:16 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# aws_cw_monitoring
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: aws_cw_monitoring
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
